1db82e3f3a74ca073128cf056b7443d842820ffe
Vlad Kotsur <kotsur.vlad@gmail.com>
2022-11-28 21:20:19 +0400

    feat: add `thunk` middleware, setup devtools for only development use

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,21 @@
-import { combineReducers, createStore } from 'redux';
-import { composeWithDevTools, devToolsEnhancer } from '@redux-devtools/extension';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevToolsDevelopmentOnly } from '@redux-devtools/extension';
 
 import { booksReducer } from './reducers';
+
+const composeEnhancers = composeWithDevToolsDevelopmentOnly({
+  traceLimit: 20,
+  trace: true,
+});
+
+let middlewares = [thunk];
+if (process.env.NODE_ENV === 'development') {
+  middlewares = [...middlewares]; // здесь можно добавить мидлвары, необходимые только development
+}
 
 const rootReducer = combineReducers({
   books: booksReducer,
 });
 
-export const store = createStore(rootReducer, devToolsEnhancer());
+export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)));
